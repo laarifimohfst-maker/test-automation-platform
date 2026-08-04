990d243dbca9158515be938bb12b037b9368f696
MohamedLaarifi <laarifimohamed715@gmail.com>
2026-08-04 20:22:16 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/testplatform/test_automation_platform/TestAutomationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.testplatform.test_automation_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testplatform/test_automation_platform/TestAutomationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testplatform.test_automation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
